1276df99eed801ba9acf1996a713b4318043d9c4
Андрей Пинтель <110303566+Pintelek@users.noreply.github.com>
2023-01-30 10:11:25 +0300

    test

--- scrypt.js
+++ scrypt.js
@@ -8,6 +8,10 @@ for(let i = 0; i < count; i++){
   creatBoard()
 }
 
+board.addEventListener("mouseover", squareHover);
+
+board.addEventListener("mouseleave", squareLeave)
+
 function getRandomColor(){
   let key = Math.floor(Math.random() * allColor.length);
   return allColor[key];
@@ -17,20 +21,24 @@ function creatBoard(){
   let square = document.createElement('div');
   square.classList.add('square')
   board.append(square);
-  square.addEventListener('mouseover', squareHover);
-  square.addEventListener('mouseleave', squareLeave);
+  //square.addEventListener('mouseover', squareHover);
+  //square.addEventListener('mouseleave', squareLeave);
 }
 
 
 
-function squareHover(){
+function squareHover(e){
+  if(e.target.classList.contains("square")){
   const color = getRandomColor();
   this.style.background = `${color}`;
   // this.style.background = `red`;
   this.style.boxShadow = `0 0 3px ${color},0 0 10px ${color}`;
+  }
 }
 
-function squareLeave(){
+function squareLeave(e){
+  if(e.target.classList.contains("square")){
   this.style.background = `rgb(41, 41, 41)`;
   this.style.boxShadow = `0 0 2px rgb(0, 0, 0)`
+  }
 }
